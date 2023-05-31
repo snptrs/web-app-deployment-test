463c0586e0d42876974bd77714baf7a018e09d58
Sean Peters <sean@snptrs.dev>
2023-05-31 18:49:42 +0100

    Update DB connection to work with Render

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -11,13 +11,20 @@ class DatabaseConnection
   # PG gem. We connect to 127.0.0.1, and select
   # the database name given in argument.
   def self.connect
-    if ENV['ENV'] == 'test'
-      database_name = 'music_library_test'
-    else
-      database_name = 'music_library_test'
+      # If the environment variable (set by Render)
+      # is present, use this to open the connection.
+      if ENV['DATABASE_URL'] != nil
+        @connection = PG.connect(ENV['DATABASE_URL'])
+        return
+      end
+    
+      if ENV['ENV'] == 'test'
+        database_name = 'music_library_test'
+      else
+        database_name = 'music_library_test'
+      end
+      @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
     end
-    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
-  end
 
   # This method executes an SQL query 
   # on the database, providing some optional parameters
